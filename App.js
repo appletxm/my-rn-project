1852480c2089f3fa0df0270b226de74dbec05635
tangxiumei <tangxiumei@rfchina.com>
2019-02-01 16:36:52 +0800

    add flex test demo

--- App.js
+++ App.js
@@ -1,65 +1,106 @@
 import React, { Component } from 'react';
-import { SectionList, StyleSheet, Text, View, FlatList } from 'react-native';
-
-export default class SectionListBasics extends Component {
-  render() {
-    return (
-      <View style={{flex: 1}}>
-        <View style={styles.container}>
-          <SectionList
-            sections={[
-              {title: 'D', data: ['Devin']},
-              {title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie', 'Julie', 'Julie', 'Julie', 'Julie', 'jjjjj']},
-            ]}
-            renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
-            renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
-            keyExtractor={(item, index) => index}
-          />
-        </View>
-
-        <View style={styles.container}>
-          <FlatList style={{marginTop: 20, backgroundColor: 'gray', color: 'white'}}
-            data={[
-              {key: 'Devin'},
-              {key: 'Jackson'},
-              {key: 'James'},
-              {key: 'Joel'},
-              {key: 'John'},
-              {key: 'Jillian'},
-              {key: 'Jimmy'},
-              {key: 'Julie'},
-            ]}
-            renderItem={({item}) => <Text style={styles.item2}>{item.key}</Text>}
-          />
-        </View>
-      </View>
-    );
-  }
-}
+import { StyleSheet, Text, View, FlatList, Alert, ActivityIndicator } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    height: 200,
-    backgroundColor: '#bbb'
-  },
-  sectionHeader: {
-    paddingTop: 2,
-    paddingLeft: 10,
-    paddingRight: 10,
-    paddingBottom: 2,
-    fontSize: 14,
-    fontWeight: 'bold',
-    backgroundColor: 'rgba(0,0,255,0.5)'
-  },
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
+    flex: 1
   },
   item2: {
-    padding: 10,
+    paddingBottom: 10,
     fontSize: 18,
     height: 44,
     color: 'white'
   }
 })
+
+export default class SectionListBasics extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      userList: [],
+      movieListObj: {
+        title: '',
+        description: '',
+        movies: [],
+      },
+      isLoading: false
+    }
+  }
+
+  componentDidMount() {
+    this.$getMovieList()
+    // this.$getUserList()
+  }
+
+  $decorateData(data) {
+    data.movies.map(item => {
+      item.key = item.id
+    })
+    return data
+  }
+
+  $getMovieList() {
+    this.setState({
+      isLoading: true
+    })
+    // Alert.alert('get movie list')
+    fetch("https://facebook.github.io/react-native/movies.json").then(response => {
+      return response.json()
+    }).then(data => {
+      // Alert.alert(JSON.stringify(data))
+      let decorateData = this.$decorateData(data)
+      this.setState({
+        movieListObj: decorateData,
+        isLoading: false
+      })
+    }).catch(err => {
+      this.setState({
+        isLoading: false
+      })
+      Alert.alert(err)
+    })
+  }
+
+  $getUserList() {
+    Alert.alert('get user list')
+    fetch("http://127.0.0.1:8019/api/rf_studio/admin/user/list", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        firstParam: "yourValue",
+        secondParam: "yourOtherValue"
+      })
+    });
+  }
+
+  $isDoding() {
+    let { title, description, movies } = this.state.movieListObj
+    let { isLoading } = this.state
+    
+    if (isLoading === true) {
+      return (
+        <View style={{backgroundColor: '#ccc', padding: 20}}><ActivityIndicator /></View>
+      )
+    } else {
+      return (<View style={styles.container}>
+        <Text style={{fontSize: 20, backgroundColor: '#EB8B00', color: '#fff', height: 40}}>{title}</Text>
+        <Text>{description}</Text>
+        <View style={{backgroundColor: '#ccc', height: 200}}>
+          <FlatList 
+            style={{marginTop: 20, backgroundColor: 'gray', color: 'white'}}
+            data={movies}
+            renderItem={({item}) => <Text style={styles.item2}>{item.title} ({item.releaseYear})</Text>}
+          />
+        </View>
+      </View>)
+    }
+  }
+
+  render() {
+    return  this.$isDoding()
+  }
+}
---
+++ src/back/App.list.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { SectionList, StyleSheet, Text, View, FlatList } from 'react-native';
+
+export default class SectionListBasics extends Component {
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <View style={styles.container}>
+          <SectionList
+            sections={[
+              {title: 'D', data: ['Devin']},
+              {title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie', 'Julie', 'Julie', 'Julie', 'Julie', 'jjjjj']},
+            ]}
+            renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+            renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+            keyExtractor={(item, index) => index}
+          />
+        </View>
+
+        <View style={styles.container}>
+          <FlatList style={{marginTop: 20, backgroundColor: 'gray', color: 'white'}}
+            data={[
+              {key: 'Devin'},
+              {key: 'Jackson'},
+              {key: 'James'},
+              {key: 'Joel'},
+              {key: 'John'},
+              {key: 'Jillian'},
+              {key: 'Jimmy'},
+              {key: 'Julie'}
+            ]}
+            renderItem={({item}) => <Text style={styles.item2}>{item.key}</Text>}
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: 200,
+    backgroundColor: '#bbb'
+  },
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(0,0,255,0.5)'
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44
+  },
+  item2: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+    color: 'white'
+  }
+})
